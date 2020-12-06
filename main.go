a533b6f106deb6bab580dd1267dfb3a03aa311cb
yoichimichael <yoichi.michael.nagano@gmail.com>
2020-12-05 21:17:04 -0500

    add cards variable and iteration

--- main.go
+++ main.go
@@ -1,18 +1,42 @@
 // will an executable-type package
 package main
 
-// unshowable, but VSCode added this automatically when saving after writing fmt.Println(card)
 import "fmt"
+
+// unshowable, but VSCode added this automatically when saving after writing fmt.Println(card)
+
+// variabel declaration without value assignment can occur outside of a func
+// var card string
 
 // every executable type package needs to contain a func called main
 func main() {
-	// value assignment to variable
+	// long form variable declaration and value assignment
 	// var card string = "Ace of Spades"
 
-	// alternative syntax
-	// Go infers data type set to variable by the value assigned to it with :=
-	card := "Ace of Spades"
 	// value reassignment doesn't use : (colon)
-	card = "Five of Diamonds"
-	fmt.Println(card)
+	// card = "Five of Diamonds"
+
+	// alternative variabel declaration syntax
+	// Go infers data type set to variable by the value assigned to it with :=
+	cards := []string{"Ace of Diamonds", newCard()}
+
+	//necessary to store result of append, often in variable holding the slice
+	cards = append(cards, "Six of Spades")
+
+	// slice iteration
+	// for i, card := range cards {
+	// 	fmt.Println(i, card)
+	// }
+
+	for index, card := range cards {
+		fmt.Println(card)
+	}
+
+	// fmt.Println(cards)
+}
+
+// return type needs to be declared
+// without 'string' after 'newCard()' the function is telling the compiler that it doesn't want to return anything (in spite of the return statement)
+func newCard() string {
+	return "Five of Diamonds"
 }
